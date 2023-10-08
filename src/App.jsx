a38aa0ea4187a54066c4cf63cb0a5e74afd20c46
mkhan141225 <safawz101@gmail.com>
2023-10-08 17:43:06 +0530

    install react-tailwind

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import {Route,Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+import About from './pages/About'
+
+
+function App() {
+ 
+
+  return (
+ 
+   <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/sign-in' element={<SignIn/>}/>
+    <Route path='/sign-up' element={<SignUp/>}/>
+    <Route path='/profile' element={<Profile/>}/>
+    <Route path='/about' element={<About/>}/>
+   </Routes>
+   
+  )
+}
+
+export default App
