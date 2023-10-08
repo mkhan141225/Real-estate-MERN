54b0187adac71c0d8b718c2ab51c28589d066904
mkhan141225 <safawz101@gmail.com>
2023-10-08 19:44:15 +0530

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
 import About from './pages/About'
+import Header from './components/Header'
 
 
 function App() {
  
 
   return (
- 
+    <>
+ <Header/>
    <Routes>
     <Route path='/' element={<Home/>}/>
     <Route path='/sign-in' element={<SignIn/>}/>
@@ -19,7 +21,7 @@ function App() {
     <Route path='/profile' element={<Profile/>}/>
     <Route path='/about' element={<About/>}/>
    </Routes>
-   
+   </>
   )
 }
 
