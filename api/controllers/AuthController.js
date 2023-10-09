aaaafd80f4a313b4a3b78302acc80c41e6135ad7
mkhan141225 <safawz101@gmail.com>
2023-10-10 00:51:08 +0530

    created form UI

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -1,21 +1,20 @@
 import User from "../models/UserModel.js";
-import bcryptjs from 'bcryptjs'
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/Error.js";
 
 
-export const signup = async (req, res) => {
+
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
-  const hashedPassword = bcryptjs.hashSync(password,10)
+  const hashedPassword = bcryptjs.hashSync(password, 10);
 
-  const newUser = new User({ username, email, password:hashedPassword });
- 
-
+  const newUser = new User({ username, email, password: hashedPassword });
 
   try {
-    await newUser.save()
-    res.status(201).json({message:"user created successfully"})
+    await newUser.save();
+    res.status(201).json({ message: "user created successfully" });
   } catch (error) {
-    res.status(500).json(error.message)
+    next(error);
   }
- 
 };
--- api/index.js
+++ api/index.js
@@ -33,3 +33,13 @@ app.listen(3000, () => {
 
  app.use("/api/user", userRouter);
  app.use("/api/auth", authRouter);
+
+
+
+ //middleware for catching error
+ app.use((err,req,res,next)=>{
+  const statusCode=err.statusCode || 500
+  const message = err.message || 'Internal Server Error'
+  return res.status(statusCode).json({success:false,statusCode,message})
+ })
+
