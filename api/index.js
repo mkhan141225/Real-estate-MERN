ceb819d96d08e1db6c051ae505ef290c4c3e1270
mkhan141225 <safawz101@gmail.com>
2023-10-09 23:58:31 +0530

    added user model

--- api/index.js
+++ api/index.js
@@ -1,11 +1,35 @@
-import express from 'express'
-import cors from "cors"
-import colors from 'colors'
+import express from "express";
+import cors from "cors";
+import colors from "colors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+ import userRouter from "./routes/UserRoutes.js";
+import authRouter from "./routes/AuthRoute.js"
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json())
 
-const app=express()
-app.use(cors())
 
-app.get('/',(req,res)=>{
-    res.send("<h1>welcome</h1>")
-})
-app.listen(3000,()=>{console.log("server is running on port 3000" .bgCyan.white)})
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to mongoDB".bgMagenta.white);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+
+
+
+
+app.get("/", (req, res) => {
+  res.json({ message: "<h1>welcome</h1>" });
+});
+app.listen(3000, () => {
+  console.log("server is running on port 3000".bgYellow.white);
+});
+
+ app.use("/api/user", userRouter);
+ app.use("/api/auth", authRouter);
