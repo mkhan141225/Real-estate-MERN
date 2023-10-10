9172ace4fa88516e6db9e4adc63bea48b2ecaed7
mkhan141225 <safawz101@gmail.com>
2023-10-10 18:34:47 +0530

    authenticat ed using jsonwebtoken

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -2,18 +2,40 @@ import User from "../models/UserModel.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/Error.js";
 
-
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-
   const hashedPassword = bcryptjs.hashSync(password, 10);
-
   const newUser = new User({ username, email, password: hashedPassword });
-
   try {
     await newUser.save();
-    res.status(201).json({ message: "user created successfully" });
+    res.status(201).json("User created successfully!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email: email });
+    if (!validUser) return next(errorHandler(404, "User is not registered"));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "wrong credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    //destructuring password so that we don't send them in token
+    const {password:pass,...rest} = validUser._doc;
+
+    res
+    .cookie("access_token", token, { 
+      httpOnly: true,
+      expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+    })
+    .status(200)
+    .json(rest)
   } catch (error) {
     next(error);
   }
--- api/index.js
+++ api/index.js
@@ -7,7 +7,18 @@ import dotenv from "dotenv";
 import authRouter from "./routes/AuthRoute.js"
 dotenv.config();
 const app = express();
-app.use(cors());
+
+
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) 
+
+
 app.use(express.json())
 
 
