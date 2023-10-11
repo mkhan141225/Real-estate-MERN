4b33a876877bc47240497c7a1d8caccc6726e0b6
mkhan141225 <safawz101@gmail.com>
2023-10-12 02:42:35 +0530

    updated header & made profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,30 @@
 import { useState } from 'react'
-import {Route,Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
 import About from './pages/About'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
 
 
 function App() {
- 
+
 
   return (
     <>
- <Header/>
-   <Routes>
-    <Route path='/' element={<Home/>}/>
-    <Route path='/sign-in' element={<SignIn/>}/>
-    <Route path='/sign-up' element={<SignUp/>}/>
-    <Route path='/profile' element={<Profile/>}/>
-    <Route path='/about' element={<About/>}/>
-   </Routes>
-   </>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+        </Route>
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </>
   )
 }
 
