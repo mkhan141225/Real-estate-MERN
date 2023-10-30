734006d20632da6eab425e7eba5d71118d116ae8
mkhan141225 <safawz101@gmail.com>
2023-10-30 18:57:49 +0530

    search functionality added

--- api/controllers/ListingController.js
+++ api/controllers/ListingController.js
@@ -54,23 +54,71 @@ export const updateListing = async (req, res, next) => {
       next(error);
     }
   } catch (error) {
-    next(errorHandler(505,"something went wrong in updateListing"))
+    next(errorHandler(505, "something went wrong in updateListing"));
   }
 };
 
-export const getListing=async(req,res,next)=>{
+export const getListing = async (req, res, next) => {
+  try {
+    //listing exist
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found"));
+    }
+
+    res.status(200).json(listing);
+  } catch (error) {
+    next(errorHandler(505, "something went wrong in getListing"));
+  }
+};
+
+
+export const getListings =async(req,res,next)=>{
 
   try {
-      //listing exist
-   const listing = await Listing.findById(req.params.id);
-  if (!listing) {
-    return next(errorHandler(404, "Listing not found"));
-  }
-  
-  res.status(200).json(listing)
+
+    const  limit = parseInt(req.query.limit) || 9
+    const startIndex = parseInt(req.query.startIndex) || 0
+
+    let offer = req.query.offer
+    if(offer === undefined || offer === 'false'){
+      offer = {$in:[false,true]}
+    }
+
+    let type = req.query.type
+    if(type === undefined || rent === 'all'){
+      type ={$in:['sale','rent']}
+    }
+
+    let furnished = req.query.furnished
+    if(furnished === undefined || furnished ==='false'){
+      furnished = {$in:[true,false]}
+    }
+
+    let parking = req.query.parking
+    if(parking === undefined || parking === 'false'){
+      parking = {$in:[true,false]}
+    }
+
+    const searchTerm = req.query.searchTerm || ''
+
+    const sort = req.query.sort || 'createdAt';
+
+    const order = req.query.order || 'desc'
     
+   const listings = await Listing.find( {
+    name :{$regex:searchTerm, $options:'i'},
+    offer,
+    furnished,
+    parking,
+    type,
+  })
+  .sort({[sort]:order})
+  .limit(limit)
+  .skip(startIndex)
+  return res.status(200).json(listings)
+
   } catch (error) {
-    next(errorHandler(505,"something went wrong in getListing"))
+    next(error)
   }
- 
 }
--- api/routes/ListingRoute.js
+++ api/routes/ListingRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createListing, deleteListing, getListing, updateListing } from '../controllers/ListingController.js'
+import { createListing, deleteListing, getListing, getListings, updateListing } from '../controllers/ListingController.js'
 import { verifyToken } from '../utils/verfiyUser.js'
 
 const router = express.Router()
@@ -8,5 +8,6 @@ router.post('/create',verifyToken,createListing)
 router.delete('/delete/:id',verifyToken,deleteListing)
 router.post('/update/:id',verifyToken,updateListing)
 router.get('/get/:id',getListing)
+router.get('/get',getListings)
 
 export default router;
